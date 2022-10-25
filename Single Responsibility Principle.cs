93aec0bee761b960e090fa129dccc39b7e166811
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-25 04:11:21 +0200

    Update Single Responsibility Principle.cs

--- Single Responsibility Principle.cs
+++ Single Responsibility Principle.cs
@@ -2,8 +2,8 @@
 Rule: Class should only have one responsibility.
 
 Example of wrong practice:
-    Class Person includes a method for checking the e-mail address. 
-    Class Person shouldn't contain attributes to the registered address.
+    1. Person class shouldn't contain an email address validation method. 
+    2. Person class shouldn't contain attributes to the registered address.
 */
 class Person
 {
@@ -14,22 +14,60 @@ class Person
     public int HouseNumber { get; set; }
     public string Email { get; set; }
     
-    public Person(string name, string lastname, string email)
+    public Person( string name, string lastname, string email )
     {
         Name = name;
         Lastname = lastname;
-        Email = ValidateEmail(email);
+        Email = ValidateEmail( email );
     }
     
-    private string ValidateEmail(string email) 
+    private string ValidateEmail( string email ) 
     {
-        if (!email.Contains("@") || !email.Contains("."))
+        if( !email.Contains( "@" ) || !email.Contains( "." ))
         {
-            throw new FormatException("Email address has a wrong format!");
+            throw new FormatException( "Email address has a wrong format!" );
         }
         return email;
     }
 }
-/*
-To avoid refactoring or code duplication, the structure should look like this:
-*/
+
+//To avoid refactoring or code duplication the stucture could look like this:
+
+class Address
+{
+    public string City { get; set; }
+    public string Street { get; set; }
+    public int HouseNumber { get; set; }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public string Email { get; set; }
+    public Address PersonAddress { get; set; }
+    
+    public Person( string name, string lastname, string email )
+    {
+        Name = name;
+        Lastname = lastname;
+        Email = email;
+    }
+}
+
+class EmailValidator
+{
+    public void ValidateEmail(string email)
+    {
+        return email;
+    }
+    public void ThrowException()
+    {
+        if( !email.Contains( "@" ) || !email.Contains( "." ))
+        {
+            throw new FormatException( "Email address has a wrong format!" );
+        }   
+    }
+}
+
+
