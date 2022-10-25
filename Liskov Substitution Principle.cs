01b3fee0500fa41abb1c0f45a64e39bfb43d52c3
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-26 00:10:02 +0200

    Update Liskov Substitution Principle.cs

--- Liskov Substitution Principle.cs
+++ Liskov Substitution Principle.cs
@@ -3,6 +3,14 @@ Rule: "Functions that use pointers or references to base classes must be able to
 IOW: We can always use any derived class in place of the base class
 or: Inheritance should be planned so that each derived class can use the functions of the base class
 
+
+The Liskov substitution principle is most often broken in the following cases:
+    - when the developer has mismanaged the inheritance mechanism, the polymorphic interface is too general,
+    - inheritance without polymorphism
+    - derived classes override the base class's methods by replacing its mismatched logic
+        - In a well-planned inheritance mechanism, derived classes should extent, not override the methods of the base classes. 
+          
+          
 In the example below, we've created the Animal abstraction, but there is a poorly thought inheritance mechanism. 
 The fish is an animal, but has the implementation of the Run() method. 
 A fish like a fish cannot run and this is a break of Liskov's substitution rule. 
