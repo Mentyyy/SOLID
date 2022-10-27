b2bcc3cf4550489250593bc61385c5badc2ac1ff
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-27 23:20:58 +0200

    Update Interface Segregation Principle.cs

--- Interface Segregation Principle.cs
+++ Interface Segregation Principle.cs
@@ -2,13 +2,19 @@
 Rule: Many dedicated interfaces are better than one too general.
 IOW: Interfaces should be specific and as small as possible. Don't create interface with unused methods.
 
-    "In C++ programming there is no built-in concept of interfaces. In order to create an interface, 
-     we need to create an abstract class which is having only pure virtual methods. 
-     In C++, Interfaces are also called pure abstract classes."
+    If we look at the interfaces built into C#, we'll see that most of them implement one or two methods:
+        IEquatable - 1 method
+        ICloneable - 1 method
+        IComparable - 1 method
+        IEnumerable - 1 method
+        IIterator - 2 methods
 
-
-The code below is incorrect because not every interface-defined method is used in derived classes.
+    However in C++ programming there is no built-in concept of interfaces. In order to create an interface, 
+    we need to create an abstract class which is having only pure virtual methods. 
+    In C++, Interfaces are also called pure abstract classes.
 */
+
+// The code below is incorrect because not every interface-defined method is used in derived classes.
 interface IRaportable
 {
     void PrintPdf();
@@ -72,12 +78,3 @@ class HighSchoolExam : IPrintablePdf
         // print Pdf here
     }
 }
-
-/*
-If we look at the interfaces built into C#, we'll see that most of them implement one or two methods:
-    IEquatable - 1 
-    ICloneable - 1 
-    IComparable - 1 
-    IEnumerable - 1 
-    IIterator - 2 
-*/
